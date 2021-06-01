23d32550f0a7527a5f0251caf995f358914a529f
Bilqis <hirosasi01@gmail.com>
2021-06-01 14:21:30 +0700

    login error solve

--- app/src/main/java/com/kei/koken/ui/LoginActivity.kt
+++ app/src/main/java/com/kei/koken/ui/LoginActivity.kt
@@ -7,72 +7,185 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.kei.koken.MainActivity
 import com.kei.koken.R
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mGoogleSignInClient : GoogleSignInClient
-    private lateinit var gso : GoogleSignInOptions
-    private lateinit var firebaseAuth : FirebaseAuth
+    private lateinit var  gso : GoogleSignInOptions
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var RC_SIGN_IN : Int = 1
 
     companion object{
-        fun getLaunchService (from: Context) = Intent(from, LoginActivity::class.java).apply{
+        fun getLaunchService(from : Context) = Intent(from, LoginActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         }
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        supportActionBar?.hide()
-        firebaseAuth = FirebaseAuth.getInstance()
 
-        tv_sign_up.setOnClickListener(this)
-        btn_signin.setOnClickListener(this)
-    }
-    override fun onStart(){
+    override fun onStart() {
         super.onStart()
         val user = FirebaseAuth.getInstance().currentUser
         if (user != null){
             startActivity(MainActivity.getLaunchService(this))
+            finish()
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
+        setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
+
+        tv_sign_up.setOnClickListener(this)
+        configurationGoogleSignIn()
+        setUpRequestUI()
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        btn_signin.setOnClickListener {
+            val email = et_email.text.toString()
+            val password = et_password.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Insert Email and Password", Toast.LENGTH_SHORT).show()
+            }
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        return@addOnCompleteListener
+                    }else{
+                        Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                }.addOnFailureListener {
+//                    Log.d("Main", "LoginFailed : ${it.message}")
+//                    Toast.makeText(this, "Email/Password incorrect", Toast.LENGTH_SHORT).show()
+                    val progressCheck = ProgressDialog(this, R.style.Theme_MaterialComponents_Light_Dialog)
+                    progressCheck.hide()
+                }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN){
+            val task : Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account)
+            }catch(e: ApiException){
+                Toast.makeText(this, "Google sign in failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount?) {
+        val credential = GoogleAuthProvider.getCredential(acct?.idToken, null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener{
+            if (it.isSuccessful){
+                startActivity(MainActivity.getLaunchService(this))
+            }else{
+                Toast.makeText(this, "Google sign in is failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun configurationGoogleSignIn() {
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("685506139442-902d365fd5stf8ha8206bei0f03p2u9l.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun setUpRequestUI() {
+        btn_google_sign_in.setOnClickListener { signIn() }
+    }
+
+    private fun signIn() {
+        val intent : Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(intent, RC_SIGN_IN)
     }
 
     override fun onClick(p0: View) {
         when(p0.id){
-            R.id.tv_sign_up -> startActivity(
-                RegisterActivity.getLaunchService(
-                    this
-                )
-            )
-            R.id.btn_signin -> loginEmailPass()
+            R.id.tv_sign_up -> startActivity(RegisterActivity.getLaunchService(this))
         }
     }
 
-    private fun loginEmailPass() {
-        val email = et_email.text.toString()
-        val password = et_password_signin.editText.toString()
-
-        if (email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Insert Complete Data", Toast.LENGTH_SHORT).show()
-        }
-
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
-                    startActivity(MainActivity.getLaunchService(this))
-                    return@addOnCompleteListener
-                } else {
-                    Toast.makeText(this, "Login Gagal", Toast.LENGTH_SHORT).show()
-                }
-            }.addOnFailureListener {
-                val progress = ProgressDialog(this, R.style.Theme_AppCompat_Light_Dialog)
-                progress.hide()
-                Toast.makeText(this, "Login Gagal", Toast.LENGTH_SHORT).show()
-            }
-    }
+//    private lateinit var mGoogleSignInClient : GoogleSignInClient
+//    private lateinit var gso : GoogleSignInOptions
+//    private lateinit var firebaseAuth : FirebaseAuth
+//
+//    companion object{
+//        fun getLaunchService (from: Context) = Intent(from, LoginActivity::class.java).apply{
+//            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+//        }
+//    }
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_login)
+//        supportActionBar?.hide()
+//        firebaseAuth = FirebaseAuth.getInstance()
+//
+//        tv_sign_up.setOnClickListener(this)
+//        btn_signin.setOnClickListener(this)
+//    }
+//
+//    override fun onStart(){
+//        super.onStart()
+//        val user = FirebaseAuth.getInstance().currentUser
+//        if (user != null){
+//            startActivity(MainActivity.getLaunchService(this))
+//        }
+//    }
+//
+//    override fun onClick(p0: View) {
+//        when(p0.id){
+//            R.id.tv_sign_up -> startActivity(
+//                RegisterActivity.getLaunchService(
+//                    this
+//                )
+//            )
+//            R.id.btn_signin -> loginEmailPass()
+//        }
+//    }
+//
+//    private fun loginEmailPass() {
+//        val email = et_email.text.toString()
+//        val password = et_password.text.toString()
+//
+//        if (email.isEmpty() || password.isEmpty()) {
+//            Toast.makeText(this, "Insert Complete Data", Toast.LENGTH_SHORT).show()
+//        }
+//
+//        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+//            .addOnCompleteListener {
+//                if (it.isSuccessful) {
+//                    Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+//                    startActivity(MainActivity.getLaunchService(this))
+//                    return@addOnCompleteListener
+//                } else {
+//                    Toast.makeText(this, "Login Gagal", Toast.LENGTH_SHORT).show()
+//                }
+//            }.addOnFailureListener {
+//                val progress = ProgressDialog(this, R.style.Theme_AppCompat_Light_Dialog)
+//                progress.hide()
+//                Toast.makeText(this, "Login Gagal", Toast.LENGTH_SHORT).show()
+//            }
+//    }
 }
